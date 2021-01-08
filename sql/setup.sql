3a3550f93814da32d351940f1f907fae9d39b7c3
dl-watson <dlarkinwatson@gmail.com>
2021-01-07 17:15:56 -0800

    add comments table

--- sql/setup.sql
+++ sql/setup.sql
@@ -14,8 +14,18 @@ CREATE TABLE posts (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   user_id BIGINT REFERENCES users(id),
   photo_url TEXT NOT NULL,
-  caption TEXT
+  caption TEXT,
+  tags TEXT[]
 );
+
+CREATE TABLE comments (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  user_id BIGINT REFERENCES users(id),
+  post_id BIGINT REFERENCES posts(id),
+  comment TEXT NOT NULL
+);
+
+-- sam: "i thought it'd be fun"
 
 CREATE TABLE tags (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
