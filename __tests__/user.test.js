79a056540850d8408b104030241fb8f76ac3bbf4
sformichella <sformichella@gmail.com>
2021-01-11 16:14:51 -0800

    add comments to user.test

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -5,13 +5,14 @@ const app = require("../lib/app");
 
 const User = require("../lib/models/User");
 const Post = require("../lib/models/Post");
+const Comment = require("../lib/models/Comment");
 
 describe("Comment Routes", () => {
   beforeEach(() => {
     return pool.query(fs.readFileSync("./sql/setup.sql", "utf-8"));
   });
 
-  let agent, users, posts;
+  let agent, users, posts, comments;
 
   beforeEach(async() => {
     const numberOfUsers = 10;
@@ -37,7 +38,33 @@ describe("Comment Routes", () => {
       );
     }));
 
-    
+    comments = await Promise.all(posts.map(postList => {
+      return Promise.all(postList.map(post => {
+        return Comment.insert({
+          postId: post.id,
+          comment: `${post.userId}-${post.id}`,
+          userId: Math.floor(Math.random() * 10 + 1)
+        });
+      }));
+    }));
+
+    console.log(comments);
+
+
+    // comments = await Promise.all(posts.map(post => {
+
+    //   const numberOfComments = [...Array(Number(post.id))];
+
+    //   return Promise.all(numberOfComments
+    //     .map((_, i) => Comment.insert({
+    //       postId: post.id,
+    //       comment: `${post.id}-${i}`,
+    //       userId: Math.floor(Math.random() * 10 + 1)
+    //     }))
+    //   );
+    // })); 
+
+    // console.log(comments);
 
   });
 
